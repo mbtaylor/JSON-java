8bc3ba000e771ddfd167248783c10676cb28ee95
Mark Taylor <m.b.taylor@bristol.ac.uk>
2024-08-27 17:41:14 +0100

    provide stable/predictable entry ordering in JSONObject
    I have made a slight local customisation of the library:
    use of HashMap in JSONObject has been replaced with LinkedHashMap,
    so that there is a predictable sequence of object keys.
    
    This shouldn't have any effect except making sure that when serializing
    a JSONObject created from a Map, the entries are written in a
    predictable order, namely the iteration order of the originating Map.
    JSON objects are supposed to be unordered, but this can provide
    a cosmetic improvement that's useful during debugging, diagnosis,
    or generally looking at serializer output.

--- src/main/java/org/json/JSONObject.java
+++ src/main/java/org/json/JSONObject.java
@@ -18,7 +18,7 @@ import java.math.BigInteger;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Enumeration;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.IdentityHashMap;
 import java.util.Iterator;
 import java.util.Locale;
@@ -172,7 +172,10 @@ public class JSONObject {
         // implementations to rearrange their items for a faster element
         // retrieval based on associative access.
         // Therefore, an implementation mustn't rely on the order of the item.
-        this.map = new HashMap<String, Object>();
+        //
+        // MBT: don't do that, use LinkedHashMap for convenience so that
+        // key order is reproducible.  This is conveneint for debugging etc.
+        this.map = new LinkedHashMap<String, Object>();
     }
 
     /**
@@ -303,9 +306,9 @@ public class JSONObject {
           throw new JSONException("JSONObject has reached recursion depth limit of " + jsonParserConfiguration.getMaxNestingDepth());
         }
         if (m == null) {
-            this.map = new HashMap<String, Object>();
+            this.map = new LinkedHashMap<String, Object>();
         } else {
-            this.map = new HashMap<String, Object>(m.size());
+            this.map = new LinkedHashMap<String, Object>(m.size());
         	for (final Entry<?, ?> e : m.entrySet()) {
         	    if(e.getKey() == null) {
         	        throw new NullPointerException("Null key.");
@@ -481,7 +484,7 @@ public class JSONObject {
      * @param initialCapacity initial capacity of the internal map.
      */
     protected JSONObject(int initialCapacity){
-        this.map = new HashMap<String, Object>(initialCapacity);
+        this.map = new LinkedHashMap<String, Object>(initialCapacity);
     }
 
     /**
@@ -2904,7 +2907,7 @@ public class JSONObject {
      * @return a java.util.Map containing the entries of this object
      */
     public Map<String, Object> toMap() {
-        Map<String, Object> results = new HashMap<String, Object>();
+        Map<String, Object> results = new LinkedHashMap<String, Object>();
         for (Entry<String, Object> entry : this.entrySet()) {
             Object value;
             if (entry.getValue() == null || NULL.equals(entry.getValue())) {
